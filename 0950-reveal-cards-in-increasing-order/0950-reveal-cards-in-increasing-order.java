1b860a0a9d591778c58b38701986e04726494a90
Shivasai Gajengi <ugs24037_cse.gajengi@cbit.org.in>
2026-07-09 16:09:39 +0530

    Time: 7 ms (94.08%), Space: 45.9 MB (45.82%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < deck.length; i++)
+        {
+            q.offer(i);
+        }
+        int ans[] = new int[deck.length];
+        for(int card : deck)
+        {
+            int idx = q.poll();
+            ans[idx] = card;
+            if(!q.isEmpty())
+            {
+                q.offer(q.poll());
+            }
+        }
+        return ans;
+    }
+}
